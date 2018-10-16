ee697eb5da3b3dd16a5474ffe5963934a50bd1f1
a073597589g <a073597589g@s09-pc07.ausias.lan>
2018-10-16 17:22:26 +0200

    "Conexión de Vibur Finalizada"

--- src/main/java/net/daw/connection/specificimplementation/HikariConnectionSpecificImplementation.java
+++ src/main/java/net/daw/connection/specificimplementation/HikariConnectionSpecificImplementation.java
@@ -9,48 +9,46 @@ import com.zaxxer.hikari.HikariDataSource;
 import net.daw.connection.publicinterface.ConnectionInterface;
 import net.daw.constant.ConnectionConstants;
 
-
-
 public class HikariConnectionSpecificImplementation implements ConnectionInterface {
 
-	private Connection oConnection;
-	private HikariDataSource oConnectionPool;
+    private Connection oConnection;
+    private HikariDataSource oConnectionPool;
 
-	public Connection newConnection() throws Exception {
+    public Connection newConnection() throws Exception {
 
-		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl(ConnectionConstants.getConnectionChain());
-		config.setUsername(ConnectionConstants.databaseLogin);
-		config.setPassword(ConnectionConstants.databasePassword);
-		config.setMaximumPoolSize(ConnectionConstants.getDatabaseMaxPoolSize);
-		config.setMinimumIdle(ConnectionConstants.getDatabaseMinPoolSize);
-		
-		config.addDataSourceProperty("cachePrepStmts", "true");
-		config.addDataSourceProperty("prepStmtCacheSize", "250");
-		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-		config.setLeakDetectionThreshold(15000);
-		config.setConnectionTestQuery("SELECT 1");
-		config.setConnectionTimeout(2000);
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(ConnectionConstants.getConnectionChain());
+        config.setUsername(ConnectionConstants.databaseLogin);
+        config.setPassword(ConnectionConstants.databasePassword);
+        config.setMaximumPoolSize(ConnectionConstants.getDatabaseMaxPoolSize);
+        config.setMinimumIdle(ConnectionConstants.getDatabaseMinPoolSize);
 
-		try {
-			oConnectionPool = new HikariDataSource(config);
-			oConnection = (Connection) oConnectionPool.getConnection();
-			return oConnection;
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.setLeakDetectionThreshold(15000);
+        config.setConnectionTestQuery("SELECT 1");
+        config.setConnectionTimeout(2000);
 
-		} catch (SQLException ex) {
-			String msgError = this.getClass().getName() + ":" + (ex.getStackTrace()[1]).getMethodName();
-			throw new Exception(msgError, ex);
-		}
+        try {
+            oConnectionPool = new HikariDataSource(config);
+            oConnection = (Connection) oConnectionPool.getConnection();
+            return oConnection;
 
-	}
+        } catch (SQLException ex) {
+            String msgError = this.getClass().getName() + ":" + (ex.getStackTrace()[1]).getMethodName();
+            throw new Exception(msgError, ex);
+        }
 
-	public void disposeConnection() throws Exception {
-		if (oConnection != null) {
-			oConnection.close();
-		}
-		if (oConnectionPool != null) {
-			oConnectionPool.close();
-		}
-	}
+    }
+
+    public void disposeConnection() throws Exception {
+        if (oConnection != null) {
+            oConnection.close();
+        }
+        if (oConnectionPool != null) {
+            oConnectionPool.close();
+        }
+    }
 
 }
--- src/main/java/net/daw/helper/EncodingHelper.java
+++ src/main/java/net/daw/helper/EncodingHelper.java
@@ -1,17 +1,18 @@
 package net.daw.helper;
 
 public class EncodingHelper {
-	public static String escapeQuotes(String str) {
-		char[] chars = str.toCharArray();
-		String strFinal = "";
-		for (int i = 0; i < chars.length; i++) {
-			if (chars[i] == '"') {
-				strFinal += String.valueOf("'");
-			} else {
-				strFinal += String.valueOf(chars[i]);
-			}
-		}
-		return String.valueOf(strFinal);
-	}
+
+    public static String escapeQuotes(String str) {
+        char[] chars = str.toCharArray();
+        String strFinal = "";
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '"') {
+                strFinal += String.valueOf("'");
+            } else {
+                strFinal += String.valueOf(chars[i]);
+            }
+        }
+        return String.valueOf(strFinal);
+    }
 
 }
