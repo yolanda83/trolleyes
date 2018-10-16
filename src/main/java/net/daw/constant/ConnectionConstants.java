b0a2a6b8111074c9a3711d089e987108565b1f0f
a073597589g <a073597589g@s09-pc07.ausias.lan>
2018-10-16 16:55:16 +0200

    "Conexión de Vibur Final"

--- src/main/java/net/daw/constant/ConnectionConstants.java
+++ src/main/java/net/daw/constant/ConnectionConstants.java
@@ -1,27 +1,25 @@
 package net.daw.constant;
 
-
-
 public class ConnectionConstants {
 
-	public static enum EnumConstans  {
-		Hikari,
-		DBCP,
-                Vibur
-	};
+    public static enum EnumConstans {
+        Hikari,
+        DBCP,
+        Vibur
+    };
 
-	public static final EnumConstans connectionPool = EnumConstans.Vibur;
-	public static final String databaseName = "trolleyes";
-	public static final String databaseLogin = "root";
-	public static final String databasePassword = "bitnami";
-	public static final String databasePort = "3306";
-	public static final String databaseHost = "localhost";
-	public static final int getDatabaseMaxPoolSize = 10;
-	public static final int getDatabaseMinPoolSize = 5;
+    public static final EnumConstans connectionPool = EnumConstans.Vibur;
+    public static final String databaseName = "trolleyes";
+    public static final String databaseLogin = "root";
+    public static final String databasePassword = "bitnami";
+    public static final String databasePort = "3306";
+    public static final String databaseHost = "localhost";
+    public static final int getDatabaseMaxPoolSize = 10;
+    public static final int getDatabaseMinPoolSize = 5;
 
-	public static String getConnectionChain() {
-		return "jdbc:mysql://" + ConnectionConstants.databaseHost + ":" + ConnectionConstants.databasePort + "/"
-				+ ConnectionConstants.databaseName;
-	}
+    public static String getConnectionChain() {
+        return "jdbc:mysql://" + ConnectionConstants.databaseHost + ":" + ConnectionConstants.databasePort + "/"
+                + ConnectionConstants.databaseName;
+    }
 
 }
--- src/main/java/net/daw/control/json.java
+++ src/main/java/net/daw/control/json.java
@@ -16,97 +16,98 @@ import net.daw.helper.EncodingHelper;
  * Servlet implementation class json
  */
 public class json extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public json() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public json() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-		String strJson = "";
-		String strOp = request.getParameter("op");
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
 
-		if (strOp != null) {
-			if (!strOp.equalsIgnoreCase("")) {
+        String strJson = "";
+        String strOp = request.getParameter("op");
 
-				if (strOp.equalsIgnoreCase("connect")) {
-					// conexion a la base de datos
+        if (strOp != null) {
+            if (!strOp.equalsIgnoreCase("")) {
 
-					try {
-						Class.forName("com.mysql.jdbc.Driver");
+                if (strOp.equalsIgnoreCase("connect")) {
+                    // conexion a la base de datos
 
-					} catch (Exception ex) {
-						strJson = "{\"status\":500,\"msg\":\"jdbc driver not found\"}";
-					}
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
 
-					try {
-						ConnectionInterface oConnectionPool = ConnectionFactory
-								.getConnection(ConnectionConstants.connectionPool);
-						Connection oConnection = oConnectionPool.newConnection();
-						// servir la petición utilizando oConnection
-						oConnectionPool.disposeConnection();
-						strJson = "{\"status\":200,\"msg\":\"Vibur Connection OK\"}";
-					} catch (Exception ex) {
-						strJson = "{\"status\":500,\"msg\":\"Bad Connection: " + EncodingHelper.escapeQuotes(ex.getMessage()) + "\"}";
-					}
+                    } catch (Exception ex) {
+                        strJson = "{\"status\":500,\"msg\":\"jdbc driver not found\"}";
+                    }
 
-				}
+                    try {
+                        ConnectionInterface oConnectionPool = ConnectionFactory
+                                .getConnection(ConnectionConstants.connectionPool);
+                        Connection oConnection = oConnectionPool.newConnection();
+                        // servir la petición utilizando oConnection
+                        oConnectionPool.disposeConnection();
+                        strJson = "{\"status\":200,\"msg\":\"Hikari Connection OK\"}";
+                    } catch (Exception ex) {
+                        strJson = "{\"status\":500,\"msg\":\"Bad Connection: " + EncodingHelper.escapeQuotes(ex.getMessage()) + "\"}";
+                    }
 
-				// http://localhost:8081/authentication/json?op=login&user=nombre&pass=password
-				// http://localhost:8081/authentication/json?op=check
-				// http://localhost:8081/authentication/json?op=logout
-				response.setContentType("application/json;charset=UTF-8");
-				HttpSession oSession = request.getSession();
+                }
 
-				if (strOp.equalsIgnoreCase("login")) {
-					String strUser = request.getParameter("user");
-					String strPass = request.getParameter("pass");
-					if (strUser.equals("rafa") && strPass.equals("thebest")) {
-						oSession.setAttribute("daw", strUser);
-						strJson = "{\"status\":200,\"msg\":\"" + strUser + "\"}";
-					} else {
-						strJson = "{\"status\":401,\"msg\":\"Authentication error\"}";
-					}
-				}
-				if (strOp.equalsIgnoreCase("logout")) {
-					oSession.invalidate();
-					strJson = "{\"status\":200,\"msg\":\"Session is closed\"}";
-				}
-				if (strOp.equalsIgnoreCase("check")) {
-					String strUserName = (String) oSession.getAttribute("daw");
-					if (strUserName != null) {
-						strJson = "{\"status\":200,\"msg\":\"" + strUserName + "\"}";
-					} else {
-						strJson = "{\"status\":401,\"msg\":\"Authentication error\"}";
-					}
-				}
-				if (strOp.equalsIgnoreCase("getsecret")) {
-					String strUserName = (String) oSession.getAttribute("daw");
-					if (strUserName != null) {
-						strJson = "{\"status\":200,\"msg\":\"985739847598\"}";
-					} else {
-						strJson = "{\"status\":401,\"msg\":\"Authentication error\"}";
-					}
-				}
+                // http://localhost:8081/authentication/json?op=login&user=nombre&pass=password
+                // http://localhost:8081/authentication/json?op=check
+                // http://localhost:8081/authentication/json?op=logout
+                response.setContentType("application/json;charset=UTF-8");
+                HttpSession oSession = request.getSession();
 
-			} else {
-				strJson = "{\"status\":200,\"msg\":\"operation empty\"}";
-			}
-		} else {
-			strJson = "{\"status\":200,\"msg\":\"operation can't be null\"}";
-		}
-		response.getWriter().append(strJson).close();
-	}
+                if (strOp.equalsIgnoreCase("login")) {
+                    String strUser = request.getParameter("user");
+                    String strPass = request.getParameter("pass");
+                    if (strUser.equals("rafa") && strPass.equals("thebest")) {
+                        oSession.setAttribute("daw", strUser);
+                        strJson = "{\"status\":200,\"msg\":\"" + strUser + "\"}";
+                    } else {
+                        strJson = "{\"status\":401,\"msg\":\"Authentication error\"}";
+                    }
+                }
+                if (strOp.equalsIgnoreCase("logout")) {
+                    oSession.invalidate();
+                    strJson = "{\"status\":200,\"msg\":\"Session is closed\"}";
+                }
+                if (strOp.equalsIgnoreCase("check")) {
+                    String strUserName = (String) oSession.getAttribute("daw");
+                    if (strUserName != null) {
+                        strJson = "{\"status\":200,\"msg\":\"" + strUserName + "\"}";
+                    } else {
+                        strJson = "{\"status\":401,\"msg\":\"Authentication error\"}";
+                    }
+                }
+                if (strOp.equalsIgnoreCase("getsecret")) {
+                    String strUserName = (String) oSession.getAttribute("daw");
+                    if (strUserName != null) {
+                        strJson = "{\"status\":200,\"msg\":\"985739847598\"}";
+                    } else {
+                        strJson = "{\"status\":401,\"msg\":\"Authentication error\"}";
+                    }
+                }
+
+            } else {
+                strJson = "{\"status\":200,\"msg\":\"operation empty\"}";
+            }
+        } else {
+            strJson = "{\"status\":200,\"msg\":\"operation can't be null\"}";
+        }
+        response.getWriter().append(strJson).close();
+    }
 
 }
