0e04923928322b539d18cc3945abc3c59a537c71
llius <llius619@gmail.com>
2018-10-26 20:49:01 +0200

    Añadido clase rellenar

---
+++ src/main/java/net/daw/service/RellenarService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.daw.service;
+
+import net.daw.bean.ProductoBean;
+
+/**
+ *
+ * @author a044531896d
+ */
+public class RellenarService {
+    
+    public ProductoBean RellenarProducto(int numero){
+        
+        return null;
+    }
+    
+}
