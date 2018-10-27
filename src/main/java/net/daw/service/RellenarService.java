65dc9adef9c838b582c347ab6029b10849185a76
llius123 <llius619@gmail.com>
2018-10-27 11:02:33 +0200

    Rellenar service acabado
    Rellenar productos devuelve un arraylist de productos, luego en service leo el arraylist.

--- src/main/java/net/daw/service/RellenarService.java
+++ src/main/java/net/daw/service/RellenarService.java
@@ -5,6 +5,7 @@
  */
 package net.daw.service;
 
+import java.util.ArrayList;
 import net.daw.bean.ProductoBean;
 
 /**
@@ -12,10 +13,35 @@ import net.daw.bean.ProductoBean;
  * @author a044531896d
  */
 public class RellenarService {
-    
-    public ProductoBean RellenarProducto(int numero){
+
+    public ArrayList<ProductoBean> RellenarProducto(int numero) {
+        String[] codigo = {"D3Q","F5Q","DP5","84D","63P"};
+        String[] desc = {"sistema", "tubo", "soporte","estación", " accesorio"};
+        String[] desc2 = {"capilar", "tubos","trabajo","coche","tren"};
+        String foto = "Foto";
+        int[] id_tipoProducto = {1, 2, 3, 4, 5};
+        int[] existencias = {1, 2, 3, 4, 5};
         
-        return null;
+        //Para tener mayor control de el maximo de objetos que tengo en los arrays
+        int maxDatos = 5;
+        
+        ArrayList<ProductoBean> resultado = new ArrayList<>();
+        ProductoBean resultadoProducto;
+        for (int i  = 0;i < numero; i++ ){
+            resultadoProducto = new ProductoBean();
+            resultadoProducto.setCodigo(codigo[randomMath(maxDatos)]);
+            resultadoProducto.setDesc(desc[randomMath(maxDatos)] + " " + desc2[randomMath(maxDatos)]);
+            //El precio sale con 4 decimales porque creo que para el cambio de divisas mola, si no pues se redondea y au
+            resultadoProducto.setPrecio((float) (Math.random()*100));
+            resultadoProducto.setFoto(foto);
+            resultadoProducto.setId_tipoProducto(id_tipoProducto[randomMath(maxDatos)]);
+            resultadoProducto.setExistencias(existencias[randomMath(maxDatos)]);
+            resultado.add(resultadoProducto);
+        }
+        return resultado;
     }
-    
+    private int randomMath(int number){
+        return (int) (Math.random()*number);
+    }
+
 }
