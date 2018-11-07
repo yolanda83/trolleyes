03d27a1c858811792440fcc7e12a623bb38be6ab
Yolanda <yolita83@gmail.com>
2018-11-07 13:59:15 +0100

    Merge upstream/master
    Conflicts:
    	pom.xml
    	src/main/java/net/daw/service/ProductoService.java
    	src/main/java/net/daw/service/UsuarioService.java

--- src/main/java/net/daw/dao/UsuarioDao.java
+++ src/main/java/net/daw/dao/UsuarioDao.java
@@ -12,7 +12,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import net.daw.bean.TipousuarioBean;
-
 import net.daw.bean.UsuarioBean;
 import net.daw.helper.SqlBuilder;
 
